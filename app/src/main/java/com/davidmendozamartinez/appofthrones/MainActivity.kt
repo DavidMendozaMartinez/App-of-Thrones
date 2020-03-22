dbd911357d6c47b0e03dda564e15ba4023c0c0da
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-03-22 19:44:44 +0100

    Add listener to MainActivity button

--- app/src/main/java/com/davidmendozamartinez/appofthrones/MainActivity.kt
+++ app/src/main/java/com/davidmendozamartinez/appofthrones/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.davidmendozamartinez.appofthrones
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Winter is coming", Toast.LENGTH_SHORT).show()
+        }
     }
 }
