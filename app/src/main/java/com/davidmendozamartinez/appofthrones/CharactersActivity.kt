428617f2e7868a978b06829ca9fefaee650adaa1
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-03-24 00:49:41 +0100

    Add intent to open DetailActivity from CharactersActivity

--- app/src/main/java/com/davidmendozamartinez/appofthrones/CharactersActivity.kt
+++ app/src/main/java/com/davidmendozamartinez/appofthrones/CharactersActivity.kt
@@ -1,12 +1,21 @@
 package com.davidmendozamartinez.appofthrones
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class CharactersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_characters)
+
+        val button: Button = findViewById(R.id.button_character)
+
+        button.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
